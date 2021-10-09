1c3090e84fbe6844b0ef43b97c9521ad8b9880ef
Robert Kirkman <robert.kirkman@ttu.edu>
2021-10-08 22:31:17 -0500

    rename files to more closely reflect their contents, refactor default memory version and add 1 new feature (either player climbing out of water no longer causes the other player to drown instantly, but drowning is still possible for both players)

--- asm_patches/main.asm
+++ asm_patches/main.asm
@@ -4,6 +4,7 @@
 ; You can also .include other ASM files into this one. For example:
 ;.include "includes/skip_intro.asm"
 ;.include "includes/bgm_file.asm"
-;.include "includes/2pmedli.asm"
-;.include "includes/2pntscworking.asm"
-.include "includes/kirbymimi_v1.0.1_ntsc_coltzero_magi.asm"
+;.include "includes/kirbymimi_v0.1_ntsc_coltzero_medli.asm"
+;.include "includes/kirbymimi_v0.5.2_ntsc_coltzero.asm"
+;.include "includes/kirbymimi_v1.0.1_ntsc_coltzero_magi_expanded_memory.asm"
+.include "includes/kirbymimi_v1.0.1_ntsc_coltzero_magi_default_memory.asm"
